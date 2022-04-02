d1bd4a3df5b57494c3460e2458a7a5b63a1cc9d6
June <cknauth119@gmail.com>
2022-04-02 20:12:51 +0000

    Update url to mainuser.dev

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -23,7 +23,7 @@ var accessToken;
 var currentOrderCanvas = document.createElement('canvas');
 var currentOrderCtx = currentOrderCanvas.getContext('2d');
 var currentPlaceCanvas = document.createElement('canvas');
-var cnc_url = 'placeie.xyz'
+var cnc_url = 'mainuser.dev'
 
 const COLOR_MAPPINGS = {
     '#BE0039': 1,
